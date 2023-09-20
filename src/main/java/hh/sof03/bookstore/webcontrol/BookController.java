f15b7caa57f283f12a7a6935f8b089386e4a5128
ehirvi <eino.hirvi@gmail.com>
2023-09-20 13:48:04 +0300

    Booklist page

--- src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
+++ src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
@@ -1,16 +1,30 @@
 package hh.sof03.bookstore.webcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.sof03.bookstore.domain.BookRepository;
 
 
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookRepository bookRep;
+
     @GetMapping("/index")
     public String frontPage() {
         return "frontpage";
+    }
+
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRep.findAll());
+        return "booklist";
     }
 
 }
