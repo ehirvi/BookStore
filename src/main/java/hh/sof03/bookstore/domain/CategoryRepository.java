32f5a2f51fed9bf6be470ade0abbd0ec91b83ac6
ehirvi <eino.hirvi@gmail.com>
2023-10-25 23:45:56 +0300

    Small fix

--- src/main/java/hh/sof03/bookstore/domain/CategoryRepository.java
+++ src/main/java/hh/sof03/bookstore/domain/CategoryRepository.java
@@ -1,7 +1,6 @@
 package hh.sof03.bookstore.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
